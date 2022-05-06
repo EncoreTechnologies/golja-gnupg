6620dab44acbb716cb34ff4794c075699b346d0d
Bradley Bishop <bradley.bishop@encore.tech>
2022-05-06 15:51:05 -0400

    updated to ensure packages.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,10 @@
 #
 class gnupg::install {
 
-  ensure_packages({ 'gnupg' =>
-    ensure => $gnupg::package_ensure,
-    name   => $gnupg::package_name,
+  ensure_packages({ 'gnupg' => {
+      ensure => $gnupg::package_ensure,
+      name   => $gnupg::package_name,
+    }
   })
 
 }
